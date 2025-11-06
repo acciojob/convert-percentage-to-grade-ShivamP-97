c8dde83848e11dfea9ce22c148ab773d6fc5d124
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-06 14:46:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function calculateGrade(percentage) {
-  //your code here
+	if(typeof percentage!=="number"|| isNaN(percentage)){
+		return "F";
+	}
+	const score=Math.round(percentage);
+	if(score>=90){
+		return "A";
+	}else if(score>="80"){
+		return "B";
+	}else if(score>="70"){
+		return "C";
+	}else if(score>="60"){
+		return "D";
+	}else{
+		return "F";
+	}
 }
 const percentage = parseInt(prompt("Enter Percentage."));
 alert(calculateGrade(percentage));
